003bab78cd9675889390272c6e5aa3b7263ee87a
Dean Stamler <dean@stamler.ca>
2019-01-11 11:13:00 -0500

    replace username field with user and upn fields

--- radiator.ps1
+++ radiator.ps1
@@ -12,8 +12,12 @@ $c_netset = Get-WmiObject Win32_NetworkAdapterConfiguration -Filter "IPEnabled =
 
 # Build the Report
 $report = [PSCustomObject]@{
-    # [System.Security.Principal.WindowsIdentity]::GetCurrent().Name returns domain prefix too
-    username = $env:UserName
+    # include domain prefix ie. DOMAIN\username
+    user = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name 
+
+    # Value() is called because otherwise it's a PropertyValueCollection Type
+    upn = ([ADSI]"LDAP://<SID=$([System.Security.Principal.WindowsIdentity]::GetCurrent().User.Value)>").UserPrincipalName.Value
+
     serial = $c_bios.SerialNumber
     os_version = $c_os.Version
     os_sku = $c_os.OperatingSystemSKU
